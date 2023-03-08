8a5602df9fea066ba100e5b3c1b32643323b30e9
Joshua Dix <juwdohr@outlook.com>
2023-03-08 16:35:08 -0700

    Added basic header and footer components, and user chat component

---
+++ frontend/src/components/FooterComponent.js
@@ -0,0 +1,5 @@
+const FooterComponent = () => {
+    return <footer>This is a footer!</footer>;
+}
+
+export default FooterComponent;
---
+++ frontend/src/components/HeaderComponent.js
@@ -0,0 +1,5 @@
+const HeaderComponent = () => {
+    return <header>This is a header</header>;
+}
+
+export default HeaderComponent;
