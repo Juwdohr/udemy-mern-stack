703ca4bd37c579463cb67c7ba39c26f4c4223f1f
Joshua Dix <juwdohr@outlook.com>
2023-03-10 18:18:03 -0700

    Updated Footer Component

--- frontend/src/components/FooterComponent.js
+++ frontend/src/components/FooterComponent.js
@@ -1,5 +1,15 @@
+import { Container, Row, Col } from 'react-bootstrap';
+
 const FooterComponent = () => {
-    return <footer>This is a footer!</footer>;
+    return (
+        <footer>
+            <Container fluid >
+                <Row className="mt-5">
+                    <Col className="text-light bg-dark text-center py-5">Copyright &copy; Joshua A. Dix</Col>
+                </Row>    
+            </Container>
+        </footer>
+    );
 }
 
 export default FooterComponent;
