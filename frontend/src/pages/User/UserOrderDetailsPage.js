e1d2c6220cdc409fe57b1da04a4b0b98a5074ed4
Joshua Dix <juwdohr@outlook.com>
2023-03-11 22:02:31 -0700

    Setup Basic Page Layout

--- frontend/src/pages/User/UserOrderDetailsPage.js
+++ frontend/src/pages/User/UserOrderDetailsPage.js
@@ -1,5 +1,30 @@
+import {
+    Container,
+    Row,
+    Col
+} from 'react-bootstrap'
+
 const UserOrderDetailsPage = () => {
-    return <p>This is a User Order Details page</p>
+    return (
+        <Container>
+            <Row className="mt-4">
+                <h1>Order Details</h1>
+                <Col md={8}>
+                    <Row>
+                        <Col md={6}>
+                            <h2>Shipping</h2>
+                        </Col>
+                        <Col md={6}>
+                            <h2>Payment Method</h2>
+                        </Col>
+                    </Row>
+                </Col>
+                <Col md={4}>
+                    <h3>Order Summary</h3>
+                </Col>
+            </Row>
+        </Container>
+    );
 }
 
 export default UserOrderDetailsPage;
