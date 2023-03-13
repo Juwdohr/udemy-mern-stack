fbe69263cf08ff1a19bd9d68d690a10d41b5b536
Joshua Dix <juwdohr@outlook.com>
2023-03-12 22:46:17 -0600

    Updated Admin pages and created components/admin/AdminLinksComponents.js

--- frontend/src/pages/Admin/AdminOrderDetailsPage.js
+++ frontend/src/pages/Admin/AdminOrderDetailsPage.js
@@ -1,5 +1,86 @@
+import {
+    Container,
+    Row,
+    Col,
+    Form,
+    Alert,
+    ListGroup,
+    Button
+} from 'react-bootstrap'
+import CartItemComponent from '../../components/CartItemComponent';
+
 const AdminOrderDetailsPage = () => {
-    return <p>This is an admins order details page.</p>
+    return (
+        <Container>
+            <Row className="mt-4">
+                <h1>Order Details</h1>
+                <Col md={8}>
+                    <Row>
+                        <Col md={6}>
+                            <h2>Shipping</h2>
+                            <b>Name</b>: John Doe <br />
+                            <b>Address</b>: 8739 Mayflower St., Los Angeles, CA 90063<br />
+                            <b>Phone</b>: (123) 456-7890
+                        </Col>
+                        <Col md={6}>
+                            <h2>Payment Method</h2>
+                            <Form.Select disable={false}>
+                                <option value="pp">PayPal</option>
+                                <option value="Cash On Delivery (Delivery may be delayed)">Cash On Delivery</option>
+
+                            </Form.Select>
+                        </Col>
+                        <Row>
+                            <Col>
+                                <Alert className="mt-3" variant="danger">
+                                    Not Delivered
+                                </Alert>
+                            </Col>
+                            <Col>
+                                <Alert className="mt-3" variant="success">
+                                    Paid on 2022-10-02
+                                </Alert>
+                            </Col>
+                        </Row>
+                        <br />
+                        <h2>Order Items</h2>
+                        <ListGroup variant="flush">
+                            {Array.from({ length: 3 }).map((item, idx) => (
+                                <CartItemComponent key={idx} />
+                            ))}
+                        </ListGroup>
+                    </Row>
+                </Col>
+                <Col md={4}>
+                    <ListGroup>
+                        <ListGroup.Item>
+                            <h3>Order Summary</h3>
+
+                        </ListGroup.Item>
+                        <ListGroup.Item>
+                            Items price (after tax): <span className="fw-bold">$892</span>
+                        </ListGroup.Item>
+                        <ListGroup.Item>
+                            Shipping: <span className="fw-bold">included</span>
+                        </ListGroup.Item>
+                        <ListGroup.Item>
+                            Tax: <span className="fw-bold">included</span>
+                        </ListGroup.Item>
+                        <ListGroup.Item className="text-danger">
+                            Total Price: <span className="fw-bold">$904</span>
+                        </ListGroup.Item>
+                        <ListGroup.Item>
+                            <div className="d-grid gap-2">
+                                <Button size="lg" variant="danger" type="button">
+                                    Mark as delivered
+                                </Button>
+                            </div>
+                        </ListGroup.Item>
+                    </ListGroup>
+                </Col>
+            </Row>
+        </Container>
+    );
 }
 
 export default AdminOrderDetailsPage;
--- frontend/src/pages/Admin/AdminProductsPage.js
+++ frontend/src/pages/Admin/AdminProductsPage.js
@@ -1,5 +1,68 @@
+import {
+    Row,
+    Col,
+    Table,
+    Button
+} from 'react-bootstrap';
+import AdminLinksComponent from '../../components/admin/AdminLinksComponent';
+import { LinkContainer } from 'react-router-bootstrap'
+const deleteHandler = () => {
+    if(window.confirm("Are you sure?")) alert("Product Deleted!");
+}
 const AdminProductsPage = () => {
-    return <p>This is an admins products page</p>
+    return (
+        <Row className="m-5">
+            <Col md={2}>
+                <AdminLinksComponent />
+            </Col>
+            <Col md={10}>
+                <h1>
+                    Product List{" "}
+                    <LinkContainer to="/admin/create-new-product">
+                        <Button variant="primary" size="lg">
+                            Create New
+                        </Button>
+                    </LinkContainer>
+                </h1>
+                <Table striped bordered hover responsive>
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Product Name</th>
+                            <th>Price</th>
+                            <th>Category</th>
+                            <th>Edit/Delete</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {[
+                            { name: "Panasonic", price: "$350", category: "TV" },
+                            { name: "Lenovo", price: "$1000", category: "Laptops" },
+                            { name: "GTA 10", price: "$345", category: "Games" }
+                        ].map((product, idx) => (
+                            <tr key={idx}>
+                                <td>{idx + 1}</td>
+                                <td>{product.name}</td>
+                                <td>{product.price}</td>
+                                <td>{product.category}</td>
+                                <td>
+                                    <LinkContainer to="admin/edit-product">
+                                        <Button className="btn-sm">
+                                            <i className="bi bi-pencil-square"></i>
+                                        </Button>
+                                    </LinkContainer>
+                                    {" / "}
+                                    <Button className="btn-sm" variant="danger" onClick={deleteHandler}>
+                                        <i className="bi bi-x-circle"></i>
+                                    </Button>
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </Table>
+            </Col>
+        </Row>
+    );
 }
 
 export default AdminProductsPage;
